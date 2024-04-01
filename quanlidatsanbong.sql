c09e5623f1d9ba4c53a6f1501bc4f28c2b1119c0
NguyenTienThiLaiBackKhoa <nguyenquoctien2401@gmail.com>
2024-04-01 15:33:21 +0700

    software

---
+++ quanlidatsanbong.sql
@@ -0,0 +1,56 @@
+
+CREATE TABLE FieldType(
+	id INT IDENTITY PRIMARY KEY,
+	name NVARCHAR(100) NOT NULL,
+	price FLOAT NOT NULL
+)
+
+CREATE TABLE FieldName(
+	id INT IDENTITY PRIMARY KEY,
+	name NVARCHAR(100) NOT NULL,
+	status NVARCHAR(100) NOT NULL,
+	idFieldType INT NOT NULL,
+	FOREIGN KEY (idFieldType) REFERENCES dbo.fieldType(id)
+)
+
+CREATE TABLE Customer(
+	id INT IDENTITY PRIMARY KEY,
+	name NVARCHAR(100) NOT NULL,
+	phone NVARCHAR(100) NOT NULL
+)
+
+CREATE TABLE CustomerBooking(
+	id INT IDENTITY PRIMARY KEY,
+	idCustomer INT NOT NULL,
+	idFieldType INT NOT NULL,
+	idFieldName INT NOT NULL,
+	startTime TIME NOT NULL,
+	endTime TIME DEFAULT GETDATE(),
+	priceBooking FLOAT NOT NULL,
+	FOREIGN KEY (idCustomer) REFERENCES dbo.Customer(id),
+	FOREIGN KEY (idFieldType) REFERENCES dbo.FieldType(id),
+	FOREIGN KEY (idFieldName) REFERENCES dbo.FieldName(id),
+)
+
+CREATE TABLE Bill(
+	id INT IDENTITY PRIMARY KEY,
+	idCustomerBooking INT NOT NULL,
+	datePayment DATETIME NOT NULL DEFAULT GETDATE(),
+	totalPrice FLOAT NOT NULL,
+	FOREIGN KEY (idCustomerBooking) REFERENCES dbo.CustomerBooking(id),
+
+
+)
+
+CREATE TABLE InformationBooking(
+	id INT IDENTITY PRIMARY KEY,
+	idBill INT NOT NULL,
+	idCustomerBooking INT NOT NULL,
+	statusBooking NVARCHAR(100) NOT NULL,
+	FOREIGN KEY (idCustomerBooking) REFERENCES dbo.CustomerBooking(id),
+	FOREIGN KEY (idBill) REFERENCES dbo.Bill(id),
+)
+
+
+
+
